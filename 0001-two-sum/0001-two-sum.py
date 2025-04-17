d0bba4553d0d33a09e6b230416e9b87bccf3c64b
priyanshu-jain1999 <70220000+jainpriyanshu77@users.noreply.github.com>
2025-04-17 19:29:09 -0400

    Time: 0 ms (100%), Space: 18.9 MB (51.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        num_map = {}  # ek khaali hash map banaya jisme number aur uski index store hogi
+        for i, num in enumerate(nums):  # array ke har element ke saath uski index bhi le rahe hain
+            compl = target - num  # hume target banane ke liye jo number chahiye, uska complement nikala
+            if compl in num_map:  # agar wo complement pehle se map me maujood hai
+                return [num_map[compl], i]  # dono index return kar do jinka sum target ban raha hai
+            num_map[num] = i  # agar nahi mila, to current number ko map me daal do future ke use ke liye
+
