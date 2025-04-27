4f5ad28401cf4b393fc4176b58701cb4567cbcb6
priyanshu-jain1999 <70220000+jainpriyanshu77@users.noreply.github.com>
2025-04-27 12:04:54 -0400

    Time: 3 ms (74.27%), Space: 12.5 MB (74.96%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        nums = map(str, nums)
+        nums.sort(cmp=lambda a, b: cmp(b + a, a + b))
+        result = "".join(nums)
+        return "0" if result[0] == '0' else result
